b619df86b4e5c8ce41e488b5069279106ba64a8c
maltamiranda <matias_lma94@hotmail.com>
2025-03-30 16:57:00 -0300

    Se determina el fin del scroll en pagina de trending para un futuro scroll infinito

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,32 +1,31 @@
-import { Component, inject } from '@angular/core';
-import { GiftListComponent } from "../../components/gift-list/gift-list.component";
+import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifService } from '../../services/gifs.service';
-
-
-/* const imageUrls: string[] = [
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-]; */
 
 
 
 @Component({
   selector: 'app-trending-page',
-  imports: [GiftListComponent],
+  imports: [],
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent {
 
   gifService = inject(GifService);
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv')
+
+  onScroll(event : Event) {
+     const scrollDiv = this.scrollDivRef()?.nativeElement;
+      if(!scrollDiv) return;
+
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeigth = scrollDiv.scrollHeight;
+
+    const isAtBottom = scrollTop + clientHeight + 200 >= scrollHeigth;
+
+
+
+
+  }
 
 }
